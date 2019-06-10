c8875cafe86dd18b06f52afc540304dcaacb1bb3
NiyazFazlyev <fazlyev@phystech.edu>
2019-06-11 00:07:17 +0500

    4.2. Максимум из двух чисел[#145334] Добавил JavaDoc.

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,6 +1,18 @@
 package ru.job4j.condition;
 
+/**
+ * Max
+ *
+ * @author Niyaz Fazlyev
+ * @since 10.06.2019
+ */
 public class Max {
+    /**
+     * Максимум двух чисел.
+     * @param left Первое число.
+     * @param right Второе число.
+     * @return Максимум двух чисел.
+     */
     public int max(int left, int right) {
         return right > left ? right : left;
     }
