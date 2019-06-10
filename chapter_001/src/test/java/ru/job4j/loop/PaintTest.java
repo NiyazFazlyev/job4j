298214f42add6db2a54b3ec6cdeaa56c00f4a521
NiyazFazlyev <fazlyev@phystech.edu>
2019-06-10 22:41:27 +0500

    5.4. Построить пирамиду в псевдографике.[#145345]

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -9,11 +9,46 @@ import static org.junit.Assert.assertThat;
 
 /**
  * @author Niyaz Fazlyev
+ * @version 2.0
  * @since 10.06.2019
  */
 public class PaintTest {
     @Test
     public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
         System.out.println(rst);
@@ -30,7 +65,7 @@ public class PaintTest {
     }
 
     @Test
-    public void whenPyramid2Right() {
+    public void whenPyramid2() {
         Paint paint = new Paint();
         String rst = paint.pyramid(2);
         System.out.println(rst);
