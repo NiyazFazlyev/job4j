5bf657d154dbda6321ea6b4872f10641a8d91519
NiyazFazlyev <fazlyev@phystech.edu>
2019-06-20 21:57:12 +0500

    2. Рефакторинг - Площадь треугольника.[#145448]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,15 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
 
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
     /**
      * Метод вычисления полупериметра по длинам сторон.
      * <p>
@@ -40,11 +48,8 @@ public class Triangle {
      *
      * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rsl = -1;
-        Point first = new Point(x1, y1);
-        Point second = new Point(x2, y2);
-        Point third = new Point(x3, y3);
         double a = first.distance(second);
         double b = second.distance(third);
         double c = third.distance(first);
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -9,13 +9,12 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
-        // Создаем объект треугольник.
-        Triangle triangle = new Triangle();
-        // Вычисляем площадь.
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
-        // Задаем ожидаемый результат.
+        Point ap = new Point(0, 0);
+        Point bp = new Point(0, 2);
+        Point cp = new Point(2, 0);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        double result = triangle.area();
         double expected = 2D;
-        //Проверяем результат и ожидаемое значение.
         assertThat(result, closeTo(expected, 0.1));
     }
 
