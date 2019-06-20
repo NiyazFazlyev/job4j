ce3dc20b492b63942e57936587cf2eaa4d62857e
NiyazFazlyev <fazlyev@phystech.edu>
2019-06-20 23:28:26 +0500

    3. Перегрузить метод max для трех чисел[#145450]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -9,11 +9,42 @@ package ru.job4j.condition;
 public class Max {
     /**
      * Максимум двух чисел.
-     * @param left Первое число.
-     * @param right Второе число.
+     * @param first Первое число.
+     * @param second Второе число.
      * @return Максимум двух чисел.
      */
-    public int max(int left, int right) {
-        return right > left ? right : left;
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    /**
+     * Максимум трех чисел.
+     *
+     * @param first  Первое число.
+     * @param second Второе число.
+     * @param third  Третье число.
+     * @return Максимум.
+     */
+    public int max(int first, int second, int third) {
+        return max(
+                first,
+                max(second, third)
+        );
+    }
+
+    /**
+     * Максимум четырех чисел.
+     *
+     * @param first  Первое число.
+     * @param second Второе число.
+     * @param third  Третье число.
+     * @param fourth Четвертое число.
+     * @return Максимум.
+     */
+    public int max(int first, int second, int third, int fourth) {
+        return max(
+                first,
+                max(second, third, fourth)
+        );
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -26,4 +26,19 @@ public class MaxTest {
         int result = max.max(2, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax154Then5() {
+        Max max = new Max();
+        int result = max.max(1, 5, 4);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax3268To2Then8() {
+        Max max = new Max();
+        int result = max.max(3, 4, 6, 8);
+        assertThat(result, is(8));
+    }
+
 }
