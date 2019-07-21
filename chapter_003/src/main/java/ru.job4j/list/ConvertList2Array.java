2b332d044eed96214910942e12185a74d3d4a0cf
NiyazFazlyev <fazlyev@phystech.edu>
2019-07-21 19:04:47 +0500

    2. Очередь с приоритетом на LinkedList[#145400]

---
+++ chapter_003/src/main/java/ru.job4j/list/ConvertList2Array.java
@@ -0,0 +1,20 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = (int) (Math.ceil((double) list.size() / (double) rows));
+        int[][] array = new int[rows][3];
+        int index = 0;
+        for (int i = 0; i != rows; i++) {
+            for (int j = 0; j != cells; j++) {
+                if (index != list.size()) {
+                    array[i][j] = list.get(index);
+                    index++;
+                }
+            }
+        }
+        return array;
+    }
+}
