e0f008972ca891b65a8a4508072cafab21944a2b
NiyazFazlyev <fazlyev@phystech.edu>
2019-08-06 15:07:10 +0500

    3. Уникальность элементов и сортировка.[#145296]

--- chapter_004/src/main/java/ru/job4j/stream/Profile.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profile.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,14 @@ public class Profile {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(n -> n.address).collect(Collectors.toList());
+    }
+
+    public static List<Address> uniqueCollect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(n -> n.address)
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
+                .collect(Collectors.toList());
     }
 
     public Address getAddress() {
--- chapter_004/src/test/java/ru/job4j/stream/ProfileTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfileTest.java
@@ -16,9 +16,25 @@ public class ProfileTest {
         Address address2 = new Address("Kazan", "Arbuzova", 5, 114);
         Profile prof1 = new Profile(address1);
         Profile prof2 = new Profile(address2);
+        System.out.println(address1.toString());
         List<Profile> profiles = new ArrayList<>(Arrays.asList(prof1, prof2));
         List<Address> expect = new ArrayList<>(Arrays.asList(address1, address2));
         List<Address> result = Profile.collect(profiles);
+        assertThat(result, is(expect));
+
+    }
+
+    @Test
+    public void whenGetUniqueAddresses() {
+        Address address1 = new Address("New York", "Victoriya", 36, 12);
+        Address address2 = new Address("Kazan", "Arbuzova", 5, 114);
+        Profile prof1 = new Profile(address1);
+        Profile prof2 = new Profile(address2);
+        Profile prof3 = new Profile(address2);
+        System.out.println(address1.toString());
+        List<Profile> profiles = new ArrayList<>(Arrays.asList(prof1, prof2, prof3));
+        List<Address> expect = new ArrayList<>(Arrays.asList(address2, address1));
+        List<Address> result = Profile.uniqueCollect(profiles);
         assertThat(result, is(expect));
 
     }
